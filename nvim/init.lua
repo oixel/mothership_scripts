32ad5602c4013ce2a0aa218b01e6687023a63711
Oixel Romero <oycell@aol.com>
2025-05-26 10:48:39 -0400

    Add shortcut for openning file explorer

--- nvim/init.lua
+++ nvim/init.lua
@@ -89,6 +89,9 @@ vim.api.nvim_create_user_command(
 	}
 )
 
+-- Makes Ctrl + B open the file explorer
+vim.api.nvim_set_keymap('n', '<C-b>, ':Explore<CR>', { noremap = true, silent = true })
+
 -- Insert Lazy vim config
 require("config.lazy")
 
