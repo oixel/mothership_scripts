435c96afae69be33f983d34107b5f4f184e72e18
Oixel Romero <oycell@aol.com>
2025-07-18 09:29:16 -0400

    style: clean up plugins

--- nvim/lua/plugins/indent-lines.lua
+++ nvim/lua/plugins/indent-lines.lua
@@ -1,5 +1,7 @@
+-- Provides proper indentation even on blank lines
+-- (https://github.com/lukas-reineke/indent-blankline.nvim)
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
