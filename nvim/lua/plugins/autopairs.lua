749cda40b65fd76d149fbfb87b1be84dae4bee1d
Oixel Romero <oycell@aol.com>
2025-07-18 09:28:47 -0400

    style: clean up plugins

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,9 @@
--- Downloads autopair plugin (https://github.com/windwp/nvim-autopairs)
+-- Add auto pairing for things such as parenthesis 
+-- (https://github.com/windwp/nvim-autopairs)
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
 }
