8e991fd0ba7358c8e63a119b84df08df91c07699
Oixel Romero <oycell@aol.com>
2025-07-18 09:29:50 -0400

    style: clean up plugins

--- nvim/lua/plugins/twilight.lua
+++ nvim/lua/plugins/twilight.lua
@@ -1,9 +1,5 @@
--- Installs Twilight plugin (https://github.com/folke/twilight.nvim?tab=readme-ov-file)
+-- Dims inactive portions of the code I am editing 
+-- (https://github.com/folke/twilight.nvim?tab=readme-ov-file)
 return {
   "folke/twilight.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
 }
